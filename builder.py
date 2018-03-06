c48a5c74a43dbdb6be025f41665afa607e6347df
Arthur Antunes <arthuragds@gmail.com>
2018-03-05 22:12:50 -0300

    Using a file to save and load a population object.

--- builder.py
+++ builder.py
@@ -19,16 +19,21 @@ depth = 500
 
 current_population = None
 
+def print_menu():
+  print "\nMENU:"
+  print "1 - Create new population."
+  print "2 - Load a saved population."
+
 def signal_handler(signal, frame):
   print "Hey buddy, you just pressed CTRL + C! Laters!"
-  savePopulation(current_population)
+  save_population(current_population)
   sys.exit(0)
 
-def savePopulation(current_population):
+def save_population(current_population):
   pickle.dump(current_population, open( "saved_population.p", "wb" ))
 
-def loadPopulation():
-  current_population = pickle.load( open( "save.p", "rb" ) )
+def load_population(path):
+  current_population = pickle.load( open( path, "rb" ) )
   return current_population
 
 def create_population(individual_num, genes_num):
@@ -86,7 +91,7 @@ def get_next_population(current_population, mutator, fitnessFunction):
 if __name__ == "__main__":
   signal.signal(signal.SIGINT, signal_handler)
 
-  target_image_path = input("Insert path to the image to be used as the target: ")
+  target_image_path = raw_input("Insert the path to the image to be used as the target: ")
 
   try:
     target_image = Image.open(target_image_path)
@@ -94,11 +99,33 @@ if __name__ == "__main__":
 
     width, height = target_image.size
 
-    number_of_indidivuals = int(input("Insert the number of images you want in the population: "))
-    number_of_genes = int(input("Insert the number of circles you want in the image: "))
-    mutation_chance = float(input("Insert the mutation chance: "))
+    #Prints the application menu.
+    print_menu()
 
-    current_population = create_population(number_of_indidivuals, number_of_genes)
+    user_option = input("What do you want to do? Type 1 or 2: ")
+
+    number_of_indidivuals = 0
+    number_of_genes = 0
+    mutation_chance = 0.0
+
+    if(user_option == 1):
+      print "\nOption is equal to 1."
+
+      number_of_indidivuals = int(input("Insert the number of images you want in the population: "))
+      number_of_genes = int(input("Insert the number of circles you want in the image: "))
+      mutation_chance = float(input("Insert the mutation chance: "))
+
+      current_population = create_population(number_of_indidivuals, number_of_genes)
+    elif(user_option == 2):
+      print "\nOption is equal to 2."
+
+      target_file_path = raw_input("Insert the path to the file on which the population is saved: ")
+      mutation_chance = float(input("Insert the mutation chance: "))
+
+      current_population = load_population(target_file_path)
+    else:
+      raise RuntimeError
+
 
     if(current_population is not None):
       ind_list = current_population.individuals
@@ -135,4 +162,7 @@ if __name__ == "__main__":
     sys.exit(1)
   except TypeError:
     print("Null object error!")
+    sys.exit(1)
+  except RuntimeError:
+    print("You must provide a valid input!")
     sys.exit(1)
